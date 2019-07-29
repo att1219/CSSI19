11fc7dcd2e37b8a553076a268df0b7761ab9a0a3
att1219 <adamthomas@mec.science>
2019-07-29 11:59:48 -0400

    msg

--- day5/script.js
+++ day5/script.js
@@ -11,3 +11,32 @@ console.log('10');
 
 // With One Line
 console.log('1\n2\n3\n4\n5\n6\n7\n8\n9\n10')
+
+// prompt
+// let result= prompt("Enter Your Name")
+// console.log("Hello", result);
+
+//let num = prompt("Enter A Number");
+//num= Number(num);
+
+//if (isNaN(num)) 
+{
+    num=10
+}
+
+// num= num +10;
+// console.log(num); 
+
+//Task 3
+let result= prompt("Enter your grade")
+let grade= Number(result);
+if (!isNaN(grade)){
+    if (grade>=70) {
+    console.log('You passed!')
+    }else{
+    console.log("You failed.")
+    }
+}else{
+    console.log("is not a numerical value")
+}
+
---
+++ day5/script2.js
@@ -0,0 +1,36 @@
+function f(x)
+{
+    return 3*x+7;
+}
+
+console.log (f(2));
+console.log (f(12));
+console.log (f(7));
+
+function g(x)
+{
+    return 5*x+5;
+}
+
+console.log (g(2))
+console.log (g(7))
+console.log (g(12))
+
+function i(x,y)
+{
+    return (x**2)-(y**2);
+}
+
+console.log("\n");
+console.log(i(3,4));
+console.log(i(4,3));
+console.log(i(5,2));
+
+
+function j(x,y,z)
+{
+    return x+y+z;
+}
+console.log(j(3,4,5))
+console.log(j(5,6,7))
+console.log(j(10,20,30))
